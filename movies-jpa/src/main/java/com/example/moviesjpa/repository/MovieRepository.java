bb874c1b89880242cd8900818ec0cb0983f4feb5
nga <=>
2022-12-31 11:28:57 +0700

    //create project spring boot use Spring data JPA

--- movies-jpa/src/main/java/com/example/moviesjpa/repository/MovieRepository.java
+++ movies-jpa/src/main/java/com/example/moviesjpa/repository/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.example.moviesjpa.repository;
 
+import com.example.moviesjpa.dto.MovieDTO;
 import com.example.moviesjpa.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +13,5 @@ public interface MovieRepository extends JpaRepository<Movie, Long> {
     @Query(value = "SELECT * FROM movie m WHERE LOWER(m.movie_name) LIKE LOWER(CONCAT('%', :name, '%'))", nativeQuery = true)
     List<Movie> findMovieByName(String name);
 
+    List<Movie> findMovieByLanguage(String language);
 }
